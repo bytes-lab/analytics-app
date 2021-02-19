823e8f962db63249a0b36c2671e600c8ee8497d2
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2021-02-19 13:44:32 -0800

    integrate opsramp api

--- hello-world-app/callbacks.py
+++ hello-world-app/callbacks.py
@@ -1,31 +1,23 @@
 import os
-import json
-import time
-
-import flask
-import requests
 
 from dash.dependencies import Input, Output, State
 
-BASE_URL = os.getenv('API_SERVER', '')
-
+from utils import *
 
 PLATFORM_ROUTE = os.getenv("PLATFORM_ROUTE", '')
 in_store_id = "_oap_data_in_" + PLATFORM_ROUTE
 out_store_id = "_oap_data_out_" + PLATFORM_ROUTE
 
+
 def register_callbacks(app):
+
     # api integration
     @app.callback(
         Output('total_clients', 'children'),
         Input(out_store_id, 'data')
     )
     def get_total_clients(data):
-        url = BASE_URL + '/msp_21998/clients/search'
-        res = requests.get(url, verify=False, cookies=flask.request.cookies)
-        total_clients = '-'
-        if res.status_code == 200:
-            total_clients = res.json()['totalResults']
+        total_clients = get_clients_count()
 
         return total_clients
 
@@ -34,10 +26,6 @@ def register_callbacks(app):
         Input(out_store_id, 'data')
     )
     def get_total_resources(data):
-        url = BASE_URL + '/msp_21998/resources/search'
-        res = requests.get(url, verify=False, cookies=flask.request.cookies)
-        total_resources = '-'
-        if res.status_code == 200:
-            total_resources = res.json()['totalResults']
+        total_resources = get_resources_count()
 
         return total_resources
---
+++ hello-world-app/utils.py
@@ -0,0 +1,39 @@
+import os
+
+import flask
+import requests
+import requests_cache
+
+requests_cache.install_cache('opsramp_cache', backend='sqlite', expire_after=3600*300, allowable_methods=("GET", "POST"))
+
+BASE_URL = os.getenv('API_SERVER', '')
+
+
+def get_msp_id():
+    url = BASE_URL + '/msps.json?_ajax=true'
+    res = requests.get(url, cookies=flask.request.cookies)
+    if res.status_code == 200:
+        if res.json()['success']:
+            return res.json()['mspId']
+
+
+def get_clients_count():
+    msp_id = get_msp_id()
+    url = BASE_URL + f'/api/v2/tenants/msp_{msp_id}/clients/search'
+    res = requests.get(url, cookies=flask.request.cookies)
+    total_clients = '-'
+    if res.status_code == 200:
+        total_clients = res.json()['totalResults']
+
+    return total_clients
+
+
+def get_resources_count():
+    msp_id = get_msp_id()
+    url = BASE_URL + f'/api/v2/tenants/msp_{msp_id}/resources/search'
+    res = requests.get(url, cookies=flask.request.cookies)
+    total_resources = '-'
+    if res.status_code == 200:
+        total_resources = res.json()['totalResults']
+
+    return total_resources
